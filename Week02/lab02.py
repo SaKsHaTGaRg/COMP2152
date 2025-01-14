5826ef7812ab620e91061af1eac354921b4daa52
Your Name <sakshat2422@gmail.com>
2025-01-14 10:58:06 -0500

    adding if-else statements

--- Week02/lab02.py
+++ Week02/lab02.py
@@ -1,5 +1,7 @@
+import random
+
 elements = ["hydrogen" , "helium" , "lithium" , "beryllium" , "boron" , "carbon"]
-# print("Elements :" , elements)
+print("Elements :" , elements)
 # # git add . && git commit -m"" && git push
 #
 # # def funct_name():
@@ -15,5 +17,21 @@ def get_valid_int_input(prompt):
         try:
             return int(input(prompt))
         except ValueError:
-            print(" Error :Please enter an integer")
+            print("Error: Please enter a valid integere!")
             continue
+
+try:
+    elements_selected = get_valid_int_input("Enter the index of the element you would like to select: ")
+    #roll the dice
+    elementRoll =  random.randint(1,6)
+    totalNum = elements_selected + elementRoll
+
+    # print the result based on the totalNum
+    if elementRoll <= 2:
+        print("You rolled a weak element, freind")
+    elif elementRoll <= 4:
+        print("your elememt is moderate")
+    else:
+        print("You rolled a strong element")
+except Exception as e:
+    print("")
