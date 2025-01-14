e48ba227e0b48bcc1c3da1c2bc35cd4f9c8ccab0
Your Name <sakshat2422@gmail.com>
2025-01-14 10:28:06 -0500

    example for coding a function

--- Week02/lab02.py
+++ Week02/lab02.py
@@ -1,6 +1,12 @@
 elements = ["hydrogen" , "helium" , "lithium" , "beryllium" , "boron" , "carbon"]
-print("Elements :" , elements)
-# git add . && git commit -m"" && git push
+# print("Elements :" , elements)
+# # git add . && git commit -m"" && git push
+#
+# # def funct_name():
+# #     return True
+#
+# def say_greeting(name , message="HI"):
+#     print(f" {message} , {name}")
+#
+# say_greeting("sakshat")
 
-# def funct_name():
-#     return True
