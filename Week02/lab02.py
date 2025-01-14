afc94f0d42071acc4e9d3e4d7694b59f7b24b4d0
Your Name <sakshat2422@gmail.com>
2025-01-14 10:23:19 -0500

    add pseudo code for function

--- Week02/lab02.py
+++ Week02/lab02.py
@@ -1,2 +1,6 @@
 elements = ["hydrogen" , "helium" , "lithium" , "beryllium" , "boron" , "carbon"]
 print("Elements :" , elements)
+# git add . && git commit -m"" && git push
+
+# def funct_name():
+#     return True
